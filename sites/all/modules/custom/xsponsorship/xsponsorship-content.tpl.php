29f42579179ab659a164d6417fcebb2abd205d1d
Wojtek Zając <wojtek@wojtekzajac.com>
2014-02-05 17:54:36 -0800

    Ad template progress

--- sites/all/modules/custom/xsponsorship/xsponsorship-content.tpl.php
+++ sites/all/modules/custom/xsponsorship/xsponsorship-content.tpl.php
@@ -13,11 +13,37 @@
  * @see template_preprocess_xsponsorship_content()
  */
 ?>
-<?php if ($image): ?>
-  <div class="block-image">
-    <?php print $image ?>
-  </div>
-<?php endif; ?>
+
+<?php
+/**
+* {% set types = {
+*     'mrec': {'width': '300', 'height': '250'},
+*     'lrec': {'width': '728', 'height': '90'},
+*     'lrec-small': {'width': '300', 'height': '50'},
+*     'small-banner': {'width': '972', 'height': '92'},
+*     'big-banner': {'width': '1336', 'height': '120'},
+*     'full-banner': {'width': '728', 'height': '90'}
+* }%}
+* 
+* {%   set ad_width = types[type]['width'] %}
+* {%   set ad_height = types[type]['height'] %}
+*/
+?>
+
+<!--<script>
+    if (typeof window.ad_rnum === 'undefined') {
+        window.ad_rnum = Math.random() * 1000000000000000000;
+    }
+    // var adSsid = fw_config();
+    var adSsid = 'idol_bts_site_chats';
+
+    ad_script = '<script src="http://FDM_AI.v.fwmrm.net/ad/g/1?nw=116457&amp;pvrn=' + ad_rnum + '&amp;csid='+adSsid.siteSection+'&amp;resp=ad;;ptgt=s&amp;envp=g_js&amp;slid={{ type }}&amp;w={{ ad_width }}&amp;h={{ ad_height }}"></scr' + 'ipt>';
+    //document.getElementById('{{ type }}').innerHTML = ad_script;
+    document.write(ad_script);
+</script>
+<noscript>
+    <iframe src="http://FDM_AI.v.fwmrm.net/ad/g/1?nw=116457&amp;pvrn=123456789&amp;csid={{ adCsid.default }}&amp;resp=ad;;ptgt=s&amp;envp=g_iframe&amp;slid={{ type }}&amp;w={{ ad_width }}&amp;h={{ ad_height }}" width="{{ ad_width }}" height="{{ ad_height }}" border="0" marginwidth="0" marginheight="0" frameborder="0" scrolling="no"></iframe>
+</noscript>-->
 
 <?php if ($content): ?>
   <div class="block-body">

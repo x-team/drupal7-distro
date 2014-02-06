6458c9acc552758d2547b1de146e57cc9006c34a
Wojtek Zając <wojtek@wojtekzajac.com>
2014-02-05 18:43:35 -0800

    TPL updates

--- sites/all/modules/custom/xsponsorship/xsponsorship-content.tpl.php
+++ sites/all/modules/custom/xsponsorship/xsponsorship-content.tpl.php
@@ -28,22 +28,42 @@
 * {%   set ad_width = types[type]['width'] %}
 * {%   set ad_height = types[type]['height'] %}
 */
+
+	$types = array(
+		'mrec' => array(
+			'width' => '300',
+			'height' => '250',
+		),
+		'lrec' => array(
+			'width' => '300',
+			'height' => '250',	
+		),
+	);
+
+	$ad_width = $types[$content]['width'];
+	$ad_height = $types[$content]['height'];
+
+	$type = 'idol_bts_site_chats';
+
 ?>
 
-<!--<script>
+<script>
     if (typeof window.ad_rnum === 'undefined') {
         window.ad_rnum = Math.random() * 1000000000000000000;
     }
     // var adSsid = fw_config();
-    var adSsid = 'idol_bts_site_chats';
+    var adSsid = <?php echo $type; ?>,
+    	ad_width  = <?php echo $ad_width; ?>,
+    	ad_height = <?php echo $ad_height; ?>;
 
-    ad_script = '<script src="http://FDM_AI.v.fwmrm.net/ad/g/1?nw=116457&amp;pvrn=' + ad_rnum + '&amp;csid='+adSsid.siteSection+'&amp;resp=ad;;ptgt=s&amp;envp=g_js&amp;slid={{ type }}&amp;w={{ ad_width }}&amp;h={{ ad_height }}"></scr' + 'ipt>';
+
+    ad_script = '<script src="http://FDM_AI.v.fwmrm.net/ad/g/1?nw=116457&amp;pvrn=' + ad_rnum + '&amp;csid=' + adSsid + '&amp;resp=ad;;ptgt=s&amp;envp=g_js&amp;slid=' + adSsid + '&amp;w=' + ad_width + 'amp;h=' + ad_height + '"></scr' + 'ipt>';
     //document.getElementById('{{ type }}').innerHTML = ad_script;
     document.write(ad_script);
 </script>
 <noscript>
-    <iframe src="http://FDM_AI.v.fwmrm.net/ad/g/1?nw=116457&amp;pvrn=123456789&amp;csid={{ adCsid.default }}&amp;resp=ad;;ptgt=s&amp;envp=g_iframe&amp;slid={{ type }}&amp;w={{ ad_width }}&amp;h={{ ad_height }}" width="{{ ad_width }}" height="{{ ad_height }}" border="0" marginwidth="0" marginheight="0" frameborder="0" scrolling="no"></iframe>
-</noscript>-->
+    <iframe src="http://FDM_AI.v.fwmrm.net/ad/g/1?nw=116457&amp;pvrn=123456789&amp;csid={{ adCsid.default }}&amp;resp=ad;;ptgt=s&amp;envp=g_iframe&amp;slid={{ type }}&amp;w=<?php echo $ad_width; ?>&amp;h=<?php echo $ad_height; ?>" width="<?php echo $ad_width; ?>" height="<?php echo $ad_height; ?>" border="0" marginwidth="0" marginheight="0" frameborder="0" scrolling="no"></iframe>
+</noscript>
 
 <?php if ($content): ?>
   <div class="block-body">
